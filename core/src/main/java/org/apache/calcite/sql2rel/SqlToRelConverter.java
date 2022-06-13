8c010946ead26b14d68551aade52f41c33e558b8
automation555 <automation555@mygamma.io>
2022-06-14 00:07:37 +0530

    AutomateCommitscanV2[CALCITE-2040] Arrow adapter (continued)

--- core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
+++ core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java
@@ -1252,7 +1252,7 @@ public class SqlToRelConverter {
             typeFactory.createSqlType(SqlTypeName.BIGINT);
         final RelNode seek = converted.r.getInput(0); // fragile
         final int keyCount = leftKeys.size();
-        final List<Integer> args = ImmutableIntList.range(0, keyCount);
+        final List<Integer> args = Util.range(keyCount);
         LogicalAggregate aggregate =
             LogicalAggregate.create(seek,
                 ImmutableList.of(),
@@ -2695,6 +2695,16 @@ public class SqlToRelConverter {
             elementType,
             validator().getValidatedNodeType(call),
             columnMappings);
+
+    final SqlValidatorScope selectScope =
+        ((DelegatingScope) bb.scope()).getParent();
+    final Blackboard seekBb = createBlackboard(selectScope, null, false);
+
+    final CorrelationUse p = getCorrelationUse(seekBb, callRel);
+    if (p != null) {
+      assert p.r instanceof LogicalTableFunctionScan;
+      callRel = (LogicalTableFunctionScan) p.r;
+    }
 
     bb.setRoot(callRel, true);
     afterTableFunction(bb, call, callRel);
@@ -4390,18 +4400,7 @@ public class SqlToRelConverter {
 
     relBuilder.push(bb.root())
         .projectNamed(exprs, fieldNames, true);
-
-    RelNode project = relBuilder.build();
-
-    final RelNode r;
-    final CorrelationUse p = getCorrelationUse(bb, project);
-    if (p != null) {
-      r = p.r;
-    } else {
-      r = project;
-    }
-
-    bb.setRoot(r, false);
+    bb.setRoot(relBuilder.build(), false);
 
     assert bb.columnMonotonicities.isEmpty();
     bb.columnMonotonicities.addAll(columnMonotonicityList);
