906a77b12982b55c49a807a69d581d5c956f5c2d
automation555 <automation555@mygamma.io>
2022-06-14 00:33:38 +0530

    AutomateCommitscanV2[CALCITE-5018]fix:Missing quoting when parser reinit

--- core/src/main/java/org/apache/calcite/sql/parser/SqlParser.java
+++ core/src/main/java/org/apache/calcite/sql/parser/SqlParser.java
@@ -18,7 +18,6 @@ package org.apache.calcite.sql.parser;
 
 import org.apache.calcite.avatica.util.Casing;
 import org.apache.calcite.avatica.util.Quoting;
-import org.apache.calcite.avatica.util.TimeUnit;
 import org.apache.calcite.config.CharLiteralStyle;
 import org.apache.calcite.config.Lex;
 import org.apache.calcite.runtime.CalciteContextException;
@@ -30,7 +29,6 @@ import org.apache.calcite.sql.validate.SqlConformanceEnum;
 import org.apache.calcite.sql.validate.SqlDelegatingConformance;
 import org.apache.calcite.util.SourceStringReader;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 import org.immutables.value.Value;
@@ -38,7 +36,6 @@ import org.immutables.value.Value;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -48,13 +45,6 @@ import java.util.Set;
 @SuppressWarnings("deprecation")
 public class SqlParser {
   public static final int DEFAULT_IDENTIFIER_MAX_LENGTH = 128;
-
-  /** Default value of {@link Config#timeUnitCodes()}.
-   * The map is empty, which means that there are no abbreviations other than
-   * the time unit names ("YEAR", "SECOND", etc.) */
-  public static final ImmutableMap<String, TimeUnit> DEFAULT_IDENTIFIER_TIMEUNIT_MAP =
-      ImmutableMap.of();
-
   @Deprecated // to be removed before 2.0
   public static final boolean DEFAULT_ALLOW_BANG_EQUAL =
       SqlConformanceEnum.DEFAULT.isBangEqualAllowed();
@@ -70,7 +60,6 @@ public class SqlParser {
     parser.setQuotedCasing(config.quotedCasing());
     parser.setUnquotedCasing(config.unquotedCasing());
     parser.setIdentifierMaxLength(config.identifierMaxLength());
-    parser.setTimeUnitCodes(config.timeUnitCodes());
     parser.setConformance(config.conformance());
     parser.switchTo(SqlAbstractParserImpl.LexicalState.forConfig(config));
   }
@@ -176,13 +165,17 @@ public class SqlParser {
    * Parses a <code>SELECT</code> statement and reuses parser.
    *
    * @param sql sql to parse
+   * @param config define the configuration for a SQL parser
    * @return A {@link org.apache.calcite.sql.SqlSelect} for a regular <code>
    * SELECT</code> statement; a {@link org.apache.calcite.sql.SqlBinaryOperator}
    * for a <code>UNION</code>, <code>INTERSECT</code>, or <code>EXCEPT</code>.
    * @throws SqlParseException if there is a parse error
    */
-  public SqlNode parseQuery(String sql) throws SqlParseException {
+  public SqlNode parseQuery(String sql, Config config) throws SqlParseException {
+    SqlAbstractParserImpl.LexicalState state = SqlAbstractParserImpl.LexicalState.forConfig(
+        config);
     parser.ReInit(new StringReader(sql));
+    parser.switchTo(state);
     return parseQuery();
   }
 
@@ -320,18 +313,6 @@ public class SqlParser {
     /** Sets {@link #charLiteralStyles()}. */
     Config withCharLiteralStyles(Iterable<CharLiteralStyle> charLiteralStyles);
 
-    /** Returns a mapping from abbreviations to time units.
-     *
-     * <p>For example, if the map contains the entry
-     * ("Y", {@link TimeUnit#YEAR}) then you can write
-     * "{@code EXTRACT(S FROM orderDate)}". */
-    @Value.Default default Map<String, TimeUnit> timeUnitCodes() {
-      return DEFAULT_IDENTIFIER_TIMEUNIT_MAP;
-    }
-
-    /** Sets {@link #timeUnitCodes()}. */
-    Config withTimeUnitCodes(Map<String, ? extends TimeUnit> timeUnitCodes);
-
     @Value.Default default SqlParserImplFactory parserFactory() {
       return SqlParserImpl.FACTORY;
     }
@@ -379,11 +360,6 @@ public class SqlParser {
 
     public ConfigBuilder setIdentifierMaxLength(int identifierMaxLength) {
       return setConfig(config.withIdentifierMaxLength(identifierMaxLength));
-    }
-
-    public ConfigBuilder setIdentifierTimeUnitMap(
-        ImmutableMap<String, TimeUnit> identifierTimeUnitMap) {
-      return setConfig(config.withTimeUnitCodes(identifierTimeUnitMap));
     }
 
     @SuppressWarnings("unused")
